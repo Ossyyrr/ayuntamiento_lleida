1c10fd35e8d3e82e0844b5dcfe78ddf98c6d9732
Patricia Manzanero <patricia.manzanero-jimenez@capgemini.com>
2024-11-24 08:14:17 +0100

    cloud firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:ayuntamiento/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
@@ -26,18 +26,32 @@ void main() async {
   String? token = await messaging.getToken();
   print("Token de notificaciones push: $token");
 
-  final database = FirebaseDatabase.instance;
-
-  try {
-    // Lee el documento 'parkings'
-    DatabaseReference parkingsRef = database.ref('parkings');
-    DataSnapshot snapshot = await parkingsRef.get();
-    print('Datos de parkings: ${snapshot.value}');
-  } catch (e) {
-    print('Error al obtener los datos de parkings: $e');
-  }
+  // Accede a todas las colecciones en Cloud Firestore
+  fetchSensorData();
 
   runApp(const MyApp());
+}
+
+void fetchSensorData() async {
+  // Referenciar el documento específico "s1" en la subcolección "sensors"
+  final sensorDoc = FirebaseFirestore.instance
+      .collection('parkings') // Colección principal
+      .doc('p1') // Documento padre
+      .collection('sensors') // Subcolección
+      .doc('s1'); // Documento específico
+
+  try {
+    // Obtener los datos del documento "s1"
+    DocumentSnapshot snapshot = await sensorDoc.get();
+
+    if (snapshot.exists) {
+      print('Datos de s1: ${snapshot.data()}');
+    } else {
+      print('El documento s1 no existe en sensors.');
+    }
+  } catch (e) {
+    print('Error al obtener los datos de s1: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
